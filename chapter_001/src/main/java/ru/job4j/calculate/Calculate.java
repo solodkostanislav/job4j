a56f5f02e32d0ae372e430540d1ceb6fa57e11ea
solodkostanislav <mobilebazar@yandex.ru>
2018-10-29 15:04:36 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
+/**
+*Calculate.
+*
+* @author Stanislav Solodko (mobilebazar@yandex.ru)
+*/
 public class Calculate {
+/**
+*Main.
+* @param args - args
+*/
 	public static void main (String[] args){
+	
 		System.out.println("Hello World");
 	}
 	
