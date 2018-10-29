bb055f8c596ab3fe803a104219eee4cbc570d047
solodkostanislav <mobilebazar@yandex.ru>
2018-10-29 17:16:16 +0300

    1.4. JUnit[#79994]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,17 +1,27 @@
 package ru.job4j.calculate;
+
 /**
-*Calculate.
-*
-* @author Stanislav Solodko (mobilebazar@yandex.ru)
-*/
+ * Calculate.
+ *
+ * @author Stanislav Solodko (mobilebazar@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
-/**
-*Main.
-* @param args - args
-*/
-	public static void main (String[] args){
-	
-		System.out.println("Hello World");
-	}
-	
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
